36b6cb4cdce771e34a9a2f0a02aaed3448e6862a
Alex Kessinger <alex@mixedmedialabs.com>
2013-07-12 18:50:06 -0700

    Notify alex@app.net on errors

--- buster/application/__init__.py
+++ buster/application/__init__.py
@@ -35,3 +35,8 @@ ADNTokenAuthMiddleware(app)
 # Werkzeug Debugger (only enabled when DEBUG=True)
 if app.debug:
     app = DebuggedApplication(app, evalex=True)
+
+import logging
+import email_logger
+
+email_logger.register_logger('alex@app.net')
