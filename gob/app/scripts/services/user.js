233ad40556682f79775a6bba97ecf482dff20f2e
Alex Kessinger <alex@mixedmedialabs.com>
2013-09-06 09:58:29 -0700

    Handle under permissions

--- gob/app/scripts/services/user.js
+++ gob/app/scripts/services/user.js
@@ -1,6 +1,6 @@
 'use strict';
 
-angular.module('pourOver').factory('LocalUser', ['$rootScope', 'LocalApiClient', 'Auth', function ($rootScope, ApiClient, Auth) {
+angular.module('pourOver').factory('LocalUser', ['$rootScope', 'LocalApiClient', 'Auth', 'ApiClient', '$location', function ($rootScope, ApiClient, Auth, UpstreamApiClient, $location) {
 
   $rootScope.current_user = {};
 
@@ -9,7 +9,21 @@ angular.module('pourOver').factory('LocalUser', ['$rootScope', 'LocalApiClient',
       url: '/me'
     }).success(function (resp) {
       $rootScope.current_user = resp.data;
+      console.log(resp.data);
     });
+
+    UpstreamApiClient.get({
+      url: '/token'
+    }).success(function (resp) {
+      var has_permissions = _.all(["messages", "write_post", "basic"], function(v){
+        return _.include(resp.data.scopes, v);
+      });
+      if (!has_permissions) {
+        window.alert('Please logout, and login again');
+        $location.path('/logout');
+      }
+    });
+
   };
 
   if (Auth.isLoggedIn()) {
