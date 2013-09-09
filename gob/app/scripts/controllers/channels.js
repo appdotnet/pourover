3052a9188f2a4dc74503ead41bdc3071abf945c0
Alex Kessinger <alex@mixedmedialabs.com>
2013-09-09 13:58:59 -0700

    Proper URL

--- gob/app/scripts/controllers/channels.js
+++ gob/app/scripts/controllers/channels.js
@@ -162,7 +162,7 @@
 
       ApiClient.createChannel(channel).success(function (data) {
         var channel = data.data;
-        var full_url = window.location.origin + "/channels/" + channel.id + '/';
+        var full_url = window.location.origin + "/alerts/" + channel.id + '/';
 
         annotations[0].value.fallback_url = full_url;
         ApiClient.updateChannel(channel, {annotations: annotations}).success(function () {
