cf0a3c13d07a9eaee35ee5e957a06f4364eb0580
Alex Kessinger <alex@mixedmedialabs.com>
2013-10-14 13:05:37 -0700

    Add in remote playground

--- buster/appengine_config.py
+++ buster/appengine_config.py
@@ -9,6 +9,7 @@ def gae_mini_profiler_should_profile_production():
     from google.appengine.api import users
     return users.is_current_user_admin()
 
+appstats_SHELL_OK = True
 
 #def webapp_add_wsgi_middleware(app):
 #    from google.appengine.ext.appstats import recording
